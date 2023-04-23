0a7757d1cf793c54324b3a8f78abaafbcda45255
Marc Kreistian Villegas <70464506+mlvilleg@users.noreply.github.com>
2023-04-22 21:56:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var button = document.getElementById("button")
+var button = document.getElementById("button");
+var button_section = document.getElementById("button_section");
 
 var alertCount = 0;
+
+
+function createButton(){
+    let btn = document.createElement("button");
+    btn.innerHTML = "Random Button";
+    btn.setAttribute('class',"btn btn-outline-info btn-xl animate__animated animate__bounce animate__repeat-2");
+    button_section.appendChild(btn);
+
+}
 
 
 button.addEventListener("mouseenter", function() {
@@ -16,7 +26,11 @@ button.addEventListener("mouseenter", function() {
     }else if(alertCount == 2){
         alert("Fine, but don't say I didn't warn you.");
         alertCount += 1;
+    }else if (alertCount ==3){
+        alertCount += 1
+        createButton();
     }
 
    
 })
+
